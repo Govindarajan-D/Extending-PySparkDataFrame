fd9af7b07bfdaef650fce1ae8555134356f73a64
Govindarajan <rdgovindarajan@gmail.com>
2022-11-25 14:40:10 +0530

    Update EnhancedDataFrame.py

--- EnhancedDataFrame/EnhancedDataFrame.py
+++ EnhancedDataFrame/EnhancedDataFrame.py
@@ -2,13 +2,12 @@ from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, lit
 
 def not_null_count(self, col_name):
-    return DataFrame(self._jdf, self.sql_ctx).filter(col(col_name).isNotNull()).count()
+    return self.filter(col(col_name).isNotNull()).count()
   
 def currency_conversion(self, to_convert, column_name):
     # We can get this from another table or API
     conversion_value = 1.05
-    df = DataFrame(self._jdf, self.sql_ctx)
-    return df.withColumn(column_name, col(to_convert) * conversion_value)
+    return self.withColumn(column_name, col(to_convert) * conversion_value)
 
 DataFrame.not_null_count = not_null_count
 DataFrame.currency_conversion = currency_conversion
